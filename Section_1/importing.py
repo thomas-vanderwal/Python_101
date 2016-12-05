3b2579ad30c0a86a4941ee079f7397290b977ab7
tvanderw <thomas.a.vanderwal@gmail.com>
2016-12-04 23:25:24 -0700

    import chapter

---
+++ Section_1/importing.py
@@ -0,0 +1,7 @@
+#import this  #This module is an easter egg that shows best practices of python
+import math
+print(math.sqrt(4))
+
+#You can specify particular modules using from ie: from math import sqrt. You will no longer need the leading math. to call sqrt
+#Another way to import is using * which imports all modules. This is considered bad practice because you can clobber
+#An object you've created with one from the module
